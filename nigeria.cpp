4c20785f8fefc5693b074fb9b081f40b2a5d0588
rajan05kr <116736352+rajan05kr@users.noreply.github.com>
2023-10-04 11:20:17 +0530

    Create nigeria.cpp

---
+++ nigeria.cpp
@@ -0,0 +1,11 @@
+body{
+    background: rgb(0, 0, 0);
+}
+
+div{
+    width: 100px;
+    height: 200px;
+    background: rgb(255, 255, 255);
+    border-left: 100px solid rgb(1, 105, 1);
+    border-right: 100px solid rgb(1, 105, 1);
+  }
